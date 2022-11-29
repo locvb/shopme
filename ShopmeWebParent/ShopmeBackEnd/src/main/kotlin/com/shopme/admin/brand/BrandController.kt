4cc4ef081cc49f086057d063169cd816421b1aae
locvb <locvb.it@gmail.com>
2022-11-29 23:57:14 +0700

    add(brand): brand form

--- ShopmeWebParent/ShopmeBackEnd/src/main/kotlin/com/shopme/admin/brand/BrandController.kt
+++ ShopmeWebParent/ShopmeBackEnd/src/main/kotlin/com/shopme/admin/brand/BrandController.kt
@@ -1,5 +1,7 @@
 package com.shopme.admin.brand
 
+import com.shopme.admin.category.CategoryService
+import com.shopme.common.entity.Brand
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -10,10 +12,20 @@ import org.springframework.web.bind.annotation.GetMapping
 class BrandController {
     @Autowired
     lateinit var brandService: BrandService;
+    @Autowired
+    lateinit var categoryService: CategoryService
 
     @GetMapping("/brands")
     fun listBrands(model: Model): String {
         model["listBrands"] = brandService.listByPage()
         return "brands/brands"
+    }
+
+    @GetMapping("/brands/new")
+    fun newBrand(model: Model): String {
+        model["pageTitle"] = "Brand"
+        model["brand"] = Brand()
+        model["listCategories"] = categoryService.listCategoriesUsedInForm()
+        return  "brands/brand_form"
     }
 }
